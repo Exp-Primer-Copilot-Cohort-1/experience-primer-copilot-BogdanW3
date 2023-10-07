469d6b7a9b5fa6cd723a267c542c30bf427232cc
Bogdan Gligorijević <bogdangligorijevic3@gmail.com>
2023-10-07 17:56:52 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,31 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMemberValue = skillsMember.value;
+    var skillsMemberText = skillsMember.options[skillsMember.selectedIndex].text;
+    var skillsMemberTextValue = skillsMemberText + " " + skillsMemberValue;
+    var skillsMemberTextValueLength = skillsMemberTextValue.length;
+    var skillsMemberTextValueLengthMax = skillsMemberTextValueLength - 1;
+    var skillsMemberTextValueLengthMax = skillsMemberTextValueLengthMax + 1;
+    var skillsMemberTextValueLengthMax = skillsMemberTextValueLengthMax + 1;
+    var skillsMemberTextValueLengthMax = skillsMemberTextValueLengthMax + 1;
+    var skillsMemberTextValueLengthMax = skillsMemberTextValueLengthMax + 1;
+    var skillsMemberTextValueLengthMax = skillsMemberTextValueLengthMax + 1;
+    var skillsMemberTextValueLengthMax = skillsMemberTextValueLengthMax + 1;
+    var skillsMemberTextValueLengthMax = skillsMemberTextValueLengthMax + 1;
+    var skillsMemberTextValueLengthMax = skillsMemberTextValueLengthMax + 1;
+    var skillsMemberTextValueLengthMax = skillsMemberTextValueLengthMax + 1;
+    if (skillsMemberTextValueLengthMax > 10) {
+        skillsMemberTextValue = skillsMemberTextValue.substring(0, 10);
+        skillsMemberTextValue = skillsMemberTextValue + "...";
+    }
+    var skillsMemberTextValueLength = skillsMemberTextValue.length;
+    var skillsMemberTextValueLengthMax = skillsMemberTextValueLengthMax - 1;
+    var skillsMemberTextValueLengthMax = skillsMemberTextValueLengthMax - 1;
+    var skillsMemberTextValueLengthMax = skillsMemberTextValueLengthMax - 1;
+    var skillsMemberTextValueLengthMax = skillsMemberTextValueLengthMax - 1;
+    var skillsMemberTextValueLengthMax = skillsMemberTextValueLengthMax - 1;
+    var skillsMemberTextValueLengthMax = skillsMemberTextValueLengthMax - 1;
+    var skillsMemberTextValueLengthMax = skillsMemberTextValueLengthMax - 1;
+    var skillsMemberTextValueLengthMax = skillsMemberTextValueLengthMax - 1;
+    var skillsMemberTextValueLengthMax = skillsMemberTextValueLengthMax - 1;
